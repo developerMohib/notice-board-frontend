6cf68f6ca08abaa0252469014ed32efa469e67a2
developerMohib <mohibullahmohim2020@gmail.com>
2026-01-13 04:59:30 +0600

    filter update

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // https://notice-board-backend-alpha.vercel.app/api
  export const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://notice-board-backend-alpha.vercel.app/api',
   timeout: 1000,
   headers: {
         'Content-Type': 'application/json',
